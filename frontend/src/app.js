35d1b00dde62f1913c66b9e9fe3e4b66ab7c6a51
Tafajjul Ansari <tafajjulans141@gmail.com>
2025-07-22 17:42:19 +0530

    Update app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import FeedPage from './pages/FeedPage';
+import PostUploadPage from './pages/PostUploadPage';
+import ProfilePage from './pages/ProfilePage';
 
 export default function App() {
   return (
@@ -9,6 +13,9 @@ export default function App() {
       <Routes>
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/feed" element={<FeedPage />} />
+        <Route path="/upload" element={<PostUploadPage />} />
+        <Route path="/profile/:id" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
